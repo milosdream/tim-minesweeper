c6d18fe23680d428362f3c95b6b9f4eb7a9af0be
App Academy Student <student@appacademy.io>
2015-10-30 12:07:44 -0700

    This is nicer to read.

---
+++ minesweeper.rb
@@ -0,0 +1,41 @@
+class Game
+  def start
+  end
+
+  def won
+  end
+end
+
+class Tile
+  def reveal
+  end
+
+  def neighbors
+  end
+
+  def neighbor_bomb_count
+  end
+end
+
+class Board
+  attr_reader :grid
+
+  def initialize(size = 3)
+    @grid = Array.new(size) {Array.new(size)}
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    grid[x][y] = value
+  end
+
+
+
+  def put_bombs(size)
+  end
+end
