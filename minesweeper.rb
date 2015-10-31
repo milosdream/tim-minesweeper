d68f84cadf00a8c448fb4fb0ad23e5c663c30ead
App Academy Student <student@appacademy.io>
2015-10-30 18:18:13 -0700

    end of day commit

--- minesweeper.rb
+++ minesweeper.rb
@@ -9,12 +9,18 @@ class Game
 end
 
 class Tile
-
+  #if tile has bomb, then it wont have anything else
+  #if tile doesnt have bomb, then it will have # or _
+  #all tiles start out will have *
+  attr_accessor :face
   def initialize(bomb = false)
+    @face = "*"
     @bomb = bomb
   end
 
+
   def reveal
+    #
   end
 
   def neighbors
@@ -31,18 +37,10 @@ class Board
     @grid = Array.new(size) {Array.new(size)}
   end
 
-  def [](x,y)
-    @grid[x][y]
-  end
-
-  def []=(x, y, value)
-    @grid[x][y] = value
-  end
-
   def put_bombs
     spots = grid.length ** 2
     tiles = []
-    (0...spots).to_a.each do |spot|
+    (1..spots).to_a.each do |spot|
       if (spot + 0.0)/spots < 0.334
         tiles << Tile.new(true)
       else
@@ -55,7 +53,7 @@ class Board
     col = (0...grid.length).to_a.shuffle!
     row.each do |x|
       col.each do |y|
-        grid.[]=(x,y,tiles[tile_number])
+        grid[x][y] = tiles[tile_number]
         tile_number += 1
       end
     end
@@ -63,22 +61,19 @@ class Board
     nil
   end
 
-  # def fill_in
-  #   tile_number = 0
-  #   row = (0...grid.length).to_a.shuffle!
-  #   col = (0...grid.length).to_a.shuffle!
-  #   row.each do |x|
-  #     col.each do |y|
-  #       grid[x][y] = tile_number
-  #       tile_number += 1
-  #     end
-  #   end
-  #   @grid
-  # end
-
 
   def display
-    @grid
+    header = (0...grid[0].length).to_a.join("  |  ")
+    puts "      #{header}"
+    puts "   #{"------" * grid[0].length}"
+
+    (0...grid.length).to_a.each do |num|
+      puts "#{num}  |  #{grid[num].join("  |  ")}"
+      puts "   #{"------" * grid[0].length}"
+    end
+    nil
   end
+
+
 
 end
